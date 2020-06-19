2e5b553c7e81b37bfba29ae1e4554d83a9fb1299
Duncan Schoenmakers <400156@student.fontys.nl>
2020-06-19 15:39:49 +0200

    changed build.gradlle for building jar

--- src/main/java/main/HibernateUtil.java
+++ src/main/java/main/HibernateUtil.java
@@ -13,7 +13,7 @@ public class HibernateUtil {
     public static Session getHibernateSession() {
 
         final SessionFactory sf = new Configuration()
-                .configure(new File("src/main/hibernate.cfg.xml")).buildSessionFactory();
+                .configure(new File("src/main/resources/hibernate.cfg.xml")).buildSessionFactory();
 
         // factory = new Configuration().configure().buildSessionFactory();
         final Session session = sf.openSession();
